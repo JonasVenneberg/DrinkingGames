88edd79859569f1655db3f6000b094a093a484bd
JonasVenneberg <jonasvenneberg@gmail.com>
2025-05-04 17:52:25 +0200

    test

---
+++ categories.js
@@ -0,0 +1,15 @@
+const categories = {
+    "Animals": [
+      "cat", "dog", "elephant", "giraffe", "tiger", "lion", "zebra", "panda", "koala", "kangaroo",
+      "hippopotamus", "rhinoceros", "buffalo", "leopard", "cheetah", "hyena", "wolf", "fox", "bear", "otter",
+      "badger", "rabbit", "deer", "moose", "antelope", "camel", "donkey", "horse", "cow", "pig",
+      "goat", "sheep", "monkey", "chimpanzee", "gorilla", "baboon", "lemur", "meerkat", "sloth", "porcupine",
+      "hedgehog", "squirrel", "beaver", "armadillo", "skunk", "platypus", "bat", "mouse", "rat", "hamster",
+      "parrot", "eagle", "hawk", "falcon", "owl", "vulture", "penguin", "flamingo", "peacock", "duck",
+      "goose", "swan", "chicken", "rooster", "turkey", "pigeon", "sparrow", "woodpecker", "toucan", "seagull",
+      "octopus", "squid", "crab", "lobster", "shrimp", "jellyfish", "starfish", "seahorse", "shark", "whale",
+      "dolphin", "seal", "walrus", "stingray", "eel", "clam", "oyster", "coral", "snail", "slug",
+      "bee", "wasp", "ant", "spider", "scorpion", "butterfly", "moth", "beetle", "grasshopper", "cricket"
+    ]
+  };
+  
